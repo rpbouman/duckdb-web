bddc0109c8c67794201a1afcd6f9199558317d4f
Franz-Kafka <spam@xj6652.de>
2026-07-14 12:12:01 +0200

    Updated Docs styles
    - Updated box-link appearance
    - Updated Docs font-sizes and headerline height
    - Updated Docs logo
    - Updated SCSS variables
    - Changed border-radius variable to figma variable
    - Fixed an issue in Docs 0.10

--- js/script.js
+++ js/script.js
@@ -487,6 +487,7 @@ $(document).ready(function(){
 	// Appending Content-List of Documentation
 	if ( $('.wrap.documentation') != 0 ) {
 	    contentlist = $('ul.sidenav').clone()
+	    contentlist.find('svg').remove()
 	    $('#docusitemaphere').append(contentlist).find("ul").removeAttr("style")
 	}
 	
